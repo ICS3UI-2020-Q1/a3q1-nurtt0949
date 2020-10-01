1f10701f4a0be23117259b07dd4c4b33770e04fb
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-01 11:10:56 -0400

    Update Main.java
    Maximum integer

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program tells the user the maximum integer
+ * @author Tolga Nurtein
  */
 public class Main {
 
@@ -9,7 +11,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //get user integer
+     System.out.println("Chose 3 integers");
+     int integer1 = input.nextInt();
+     int integer2 = input.nextInt();
+     int integer3 = input.nextInt();
+
+    //determine which number is greater
+      if (integer1 > integer2 && integer1 > integer3){
+      System.out.println("The maximum number of your choices was " + integer1);
     
-  }
-}
+      }else if (integer2 > integer1 && integer2 > integer3){
+      System.out.println("The maximum number of your choices was " + integer2);
+    }
+    else if (integer3 > integer1 && integer3 > integer2){
+      System.out.println("The maximum number of your choices was " + integer3);
+    }
+    }
+    }
+    
+
